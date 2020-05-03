09f18d3c74594c7f872e757f8cb1d8a3e46029af
Gabriel Coelho <gabriellvs60@gmail.com>
2020-05-03 18:29:24 -0300

    :lipstick: cart screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/screens/cart_screen.dart';
 import 'package:flutter_food_delivery_ui/screens/restaurant_screen.dart';
 import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
 import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
@@ -107,8 +108,13 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontSize: 20.0,
                   ),
                 ),
-                onPressed: () {},
-              )
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CartScreen(),
+                  ),
+                ),
+              ),
             ]),
         body: ListView(
           children: <Widget>[
